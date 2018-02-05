d21b30f55464e516132154e3852c031560fe5ef2
limingming <limingming@smartisan>
2018-02-05 22:49:09 +0800

    链式存储结构

--- src/大话数据结构/第3章_线性表/ChainedStorage.java
+++ src/大话数据结构/第3章_线性表/ChainedStorage.java
@@ -4,4 +4,20 @@ package 大话数据结构.第3章_线性表;
  * Created by xql on 2018/2/5.
  */
 public class ChainedStorage {
+
+
+    /**
+     * 定义一个节点
+     * @param <E>
+     */
+    private static class Node<E>{
+            E item;
+            Node<E> next;
+            Node<E> prev;
+            Node(Node<E> prev, E element, Node<E> next) {
+                this.item = element;
+                this.next = next;
+                this.prev = prev;
+            }
+        }
 }
