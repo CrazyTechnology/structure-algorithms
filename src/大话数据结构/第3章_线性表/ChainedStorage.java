d1f34e9198d9523cd96111380f664a8f84b49a07
limingming <limingming_0305@163.com>
2018-02-05 23:09:15 +0800

    大话数据结构

--- src/大话数据结构/第3章_线性表/ChainedStorage.java
+++ src/大话数据结构/第3章_线性表/ChainedStorage.java
@@ -3,7 +3,30 @@ package 大话数据结构.第3章_线性表;
 /**
  * Created by xql on 2018/2/5.
  */
-public class ChainedStorage {
+public class ChainedStorage <E>{
+
+
+
+    public  E  getElement(E element){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
