62329f36576613910788c9713216d2a87316ed59
liming <limingming_0305@163.com>
2018-02-21 17:36:08 +0800

    棧

--- src/大话数据结构/第4章_栈与队列/ChainedStorageStack.java
+++ src/大话数据结构/第4章_栈与队列/ChainedStorageStack.java
@@ -1,9 +1,29 @@
 package 大话数据结构.第4章_栈与队列;
 
+import java.util.Stack;
+
 /**
  * Created by ming on 18-2-9.
  * 栈的链式存储
  */
 
-public class ChainedStorageStack {
+public class ChainedStorageStack<E> {
+
+    protected int elementCount;
+    protected Object[] elementData;
+    public ChainedStorageStack() {
+    }
+    public E push(E item) {
+        addElement(item);
+
+        return item;
+    }
+
+    /**
+     * 添加元素
+     * @param item
+     */
+    private synchronized void addElement(E item) {
+        elementData[elementCount++] = item;
+    }
 }
