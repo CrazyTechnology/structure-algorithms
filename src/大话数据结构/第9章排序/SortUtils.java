b7520838f8beb7a16e7ecf97077b59123273bc6f
limingming <limingming_0305@163.com>
2018-08-10 16:25:49 +0800

    提交课后题

---
+++ src/大话数据结构/第9章排序/SortUtils.java
@@ -0,0 +1,47 @@
+package 大话数据结构.第9章排序;
+
+
+import java.lang.reflect.Array;
+
+public class SortUtils {
+
+    /**
+     * 快速排序
+     */
+    public static void QuickSort(int [] arrays){
+        qsort(arrays,1,arrays.length);
+    }
+
+    private static void qsort(int[] arrays, int low, int higth) {
+        int pivot;
+        if(low<higth){
+            pivot=partition(arrays,low,higth);
+            qsort(arrays,low,pivot-1);
+            qsort(arrays,pivot+1,higth);
+        }
+    }
+
+
+
+    //获取枢轴位置
+    private static int partition(int[] arrays, int low, int higth) {
+        int pivotkey;
+        pivotkey=arrays[low];
+        while (low<higth){
+            while (low<higth&&arrays[higth]>=pivotkey)
+                higth--;
+            swap(arrays,low,higth);
+            while (low<higth&&arrays[low]<=pivotkey)
+                low++;
+            swap(arrays,low,higth);
+
+        }
+        return low; //返回枢轴所在位置
+
+    }
+
+    //交换位置
+    private static void swap(int[] arrays, int low, int higth) {
+    }
+
+}
